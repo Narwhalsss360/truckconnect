4e8e7398126f48a2503b264be16bcc7b0da03be2
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-11 21:51:05 -0400

    Added platform sockets

--- c++/truckconnect/include/truck_connect_platform_win/includes.h
+++ c++/truckconnect/include/truck_connect_platform_win/includes.h
@@ -1,2 +1,3 @@
 #pragma once
 #include "truckconnect_platform_win_event_signal.h"
+#include "truckconnect_platform_win_sockets.h"
---
+++ c++/truckconnect/include/truck_connect_platform_win/truckconnect_platform_win_sockets.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "truckconnect_platform_win.h"
+
+namespace truckconnect {
+    namespace platform {
+        namespace sockets {
+            using error_int = int;
+
+            using socket = SOCKET;
+
+            static bool initiaize() {
+                WSADATA data;
+                return WSAStartup(MAKEWORD(2, 2), &data) == 0;
+            }
+            
+            static error_int last_error() {
+                return WSAGetLastError();
+            }
+
+            static bool nonblocking(socket socket) {
+                u_long mode = 1;
+                return ioctlsocket(socket, FIONBIO, &mode);
+            }
+            
+            static bool deinitialize() {
+                return WSACleanup() == 0;
+            }
+
+            namespace errors {
+                constexpr const error_int SE_EWOULDBLOCK = WSAEWOULDBLOCK;
+                
+                constexpr const error_int SE_EAGAIN = EAGAIN;
+
+                constexpr const error_int SE_ECONNRESET = WSAECONNRESET;
+
+                constexpr const error_int SE_EALREADY = WSAEALREADY;
+            }
+        }
+    }
+}
