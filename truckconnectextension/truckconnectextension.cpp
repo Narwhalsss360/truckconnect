db0116437e5b4c90e09f508cd975c720b6b2aed1
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-14 19:38:14 -0400

    Limiting print of game thread lap

--- truckconnectextension/truckconnectextension.cpp
+++ truckconnectextension/truckconnectextension.cpp
@@ -2,6 +2,7 @@
 #include "register_all.h"
 #include "clients.h"
 #include <scssdk/scssdk_telemetry_event.h>
+#include <chrono>
 
 using std::string;
 using std::to_string;
@@ -45,10 +46,18 @@ SCSAPI_VOID started(scs_event_t event, const void* const event_info, scs_context
 
 SCSAPI_VOID frame_end(scs_event_t event, const void* const event_info, scs_context_t context) {
     using namespace truckconnect::platform::event_signal;
+    constexpr const std::clock_t NOTIFY_LAP_INTERVAL = 5000;
+    static std::clock_t last_lap = {};
+    static uint32_t laps = 0;
 
     switch (signaled(frame_end_signal())) {
     case signal_state::signaled:
-        console_log(SCS_LOG_TYPE_warning, IDENTSTR(frame_end), "Game thread lapped dispatcher.");
+        laps++;
+        if (std::clock() - last_lap >= NOTIFY_LAP_INTERVAL) {
+            console_log(SCS_LOG_TYPE_warning, IDENTSTR(frame_end), "Game thread lapped dispatcher thread " + to_string(laps) + " times in the last " + to_string(NOTIFY_LAP_INTERVAL) + "ms.");
+            laps = 0;
+            last_lap = std::clock();
+        }
     case signal_state::not_signaled:
         if (!set(frame_end_signal())) {
             console_log(SCS_LOG_TYPE_error, IDENTSTR(frame_end), "Signal set(...) error: " + to_string(last_error()));
