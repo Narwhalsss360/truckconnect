9619d60c3524cb1f7c06fb22631ac30d35f052c4
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-17 00:25:35 -0400

    Implemented basic telemetryid request

--- C#/TestClientDotNet/Program.cs
+++ C#/TestClientDotNet/Program.cs
@@ -2,5 +2,14 @@
 
 Connection connection = new("127.0.0.1");
 connection.Connect();
+while (true)
+{
+    connection.SendRequestForAsync(TelemetryID.ChannelGameTime).Wait();
+    connection.ReceiveAllAsync().Wait();
+    bool initialzied = connection.Collector.Data[Connection.TELEMTRY_DATA_START] > 0;
+    UInt32 gameTime = BitConverter.ToUInt32(connection.Collector.Data, Connection.TELEMTRY_DATA_START + 1);
+
+    Console.WriteLine(initialzied ? gameTime : "---");
+}
 connection.Disconnect();
 
--- C#/TruckConnectDotNet/Connection.cs
+++ C#/TruckConnectDotNet/Connection.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Sockets;
 using NStreamCom;
 
@@ -8,11 +9,37 @@ namespace TruckConnect
     {
         public static readonly ushort PORT = 52878;
 
+        public static readonly long RECEIVE_ALL_BUFFER_SIZE = 32;
+
+        public static readonly int TELEMTRY_DATA_START = 1 + 2;
+
+        public static readonly int DATA_DEFINITION_DATA_START = 1 + 1;
+
+        public static readonly int DEFINED_DATA_DATA_START = 1 + 1;
+
+        public enum RequestType
+        {
+            None,
+            TelemetryID,
+            RegisterDataDefinition,
+            DefinedData,
+            UnregisterDataDefinition,
+            ErrorResponse
+        }
+
         readonly private Socket m_socket;
 
         readonly private IPAddress m_ipAddress;
 
-        readonly private Collector m_collector = new();
+        readonly public Collector Collector = new();
+
+        private TelemetryID m_pendingID = TelemetryID.Invalid;
+
+        private TrailerIndexOrCount m_pendingTrailerIndexOrCount = new();
+
+        public RequestType PendingRequest { get; private set; } = RequestType.None;
+
+        public bool Connected { get => m_socket.Connected; }
 
         public Connection(string ip)
         {
@@ -30,9 +57,67 @@ namespace TruckConnect
             ConnectAsync().Wait();
         }
 
+        public async Task SendRequestForAsync(TelemetryID id, TrailerIndexOrCount trailerIndexOrCount = default, CancellationToken cancellationToken = default)
+        {
+            EnsureConnected(nameof(SendRequestForAsync));
+            EnsurePendingRequest(RequestType.None, "Request already pending.");
+            await m_socket.SendAsync(
+                NEncode.EncodeWithSize([
+                    (byte)RequestType.TelemetryID,
+                    (byte)id,
+                    trailerIndexOrCount.AsByte()
+                ]),
+                cancellationToken
+            );
+            PendingRequest = RequestType.TelemetryID;
+            m_pendingID = id;
+            m_pendingTrailerIndexOrCount = trailerIndexOrCount;
+        }
+
+        public void ReceiveOne()
+        {
+            EnsureConnected(nameof(ReceiveOne));
+            EnsureAnyPendingRequest();
+            byte[] buffer = new byte[1];
+            m_socket.Receive(buffer);
+            Collector.Collect(buffer[0]);
+        }
+
+        public async Task ReceiveAllAsync()
+        {
+            EnsureConnected(nameof(ReceiveOne));
+            EnsureAnyPendingRequest();
+            do
+            {
+                byte[] buffer = new byte[1];
+                await m_socket.ReceiveAsync(buffer);
+                Collector.Collect(buffer[0]);
+            } while (Collector.State == Collector.States.WaitingSize || Collector.State == Collector.States.WaitingData);
+            PendingRequest = RequestType.None;
+        }
+
         public void Disconnect()
         {
+            Collector.Reset();
             m_socket.Close();
+        }
+
+        private void EnsureConnected(string operationName)
+        {
+            if (!Connected)
+                throw new InvalidOperationException($"This operation ({operationName}) connection to be connected.");
+        }
+
+        private void EnsurePendingRequest(RequestType request, string errorMessage)
+        {
+            if (PendingRequest != request)
+                throw new InvalidOperationException(errorMessage);
+        }
+
+        private void EnsureAnyPendingRequest()
+        {
+            if (PendingRequest == RequestType.None)
+                throw new InvalidOperationException("There is no pending request.");
         }
     }
 }
