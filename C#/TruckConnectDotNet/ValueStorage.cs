e82456c144e79ad67d030f61fd2c4e404458c82c
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-16 21:52:27 -0400

    Added ValueStorage

---
+++ C#/TruckConnectDotNet/ValueStorage.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace TruckConnect
+{
+    struct ValueStorage<T> where T : new()
+    {
+        public bool Initialized;
+
+        public T Value;
+
+        public ValueStorage()
+        {
+            Initialized = false;
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Value = new();
+        }
+    }
+
+    struct ValueArrayStorage<T> where T : new()
+    {
+        public bool Initialized;
+
+        public T[] Values;
+
+        public ValueArrayStorage(UInt32 maxCount)
+        {
+            Initialized = false;
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Values = new T[maxCount];
+        }
+    }
+
+    struct ValueListStorage<T> where T : new()
+    {
+        public List<T> Values;
+
+        public ValueListStorage()
+        {
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Values = new();
+        }
+    }
+}
