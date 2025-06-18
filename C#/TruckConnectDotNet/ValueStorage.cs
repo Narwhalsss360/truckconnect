e055129dbbe50c15bb8c23b5536b1cc5c80b3377
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-17 22:33:52 -0400

    Added value constructors

--- C#/TruckConnectDotNet/ValueStorage.cs
+++ C#/TruckConnectDotNet/ValueStorage.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-
-namespace TruckConnect
+﻿namespace TruckConnect
 {
     public struct ValueStorage<T>
     {
@@ -15,13 +13,20 @@ namespace TruckConnect
             if (typeof(T) == typeof(String))
             {
                 Value = (T)("" as object);
-        }
+            }
             else
             {
                 if (Activator.CreateInstance(typeof(T)) is not T value)
                     throw new NotImplementedException();
                 Value = value;
-    }
+            }
+        }
+
+        public ValueStorage(bool initialized, T value)
+        {
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Initialized = initialized;
+            Value = value;
         }
     }
 
@@ -37,6 +42,13 @@ namespace TruckConnect
             SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
             Values = new T[maxCount];
         }
+
+        public ValueArrayStorage(bool initialized, T[] values)
+        {
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Initialized = initialized;
+            Values = values;
+        }
     }
 
     public struct ValueListStorage<T>
@@ -47,6 +59,12 @@ namespace TruckConnect
         {
             SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
             Values = new();
+        }
+
+        public ValueListStorage(List<T> values)
+        {
+            SCSValueTypeFunctions.GetSCSValueTypeOf<T>();
+            Values = values;
         }
     }
 }
