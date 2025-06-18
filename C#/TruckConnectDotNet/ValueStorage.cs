34aba5c84daf189e2287db6fe9266a9cfd2c42cc
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-17 22:35:07 -0400

    Implemented ValueStorage for only channels

--- C#/TruckConnectDotNet/ValueStorage.cs
+++ C#/TruckConnectDotNet/ValueStorage.cs
@@ -67,4 +67,94 @@
             Values = values;
         }
     }
+
+    public static class ValueStorageFunctions
+    {
+        public static Type? GetGenericStorageTypeDefinition(this Type type)
+        {
+             if (!type.IsGenericType)
+                return null;
+
+            Type genericDefinition = type.GetGenericTypeDefinition();
+            if (genericDefinition == typeof(ValueStorage<>))
+                return typeof(ValueStorage<>);
+            else if (genericDefinition == typeof(ValueArrayStorage<>))
+                return typeof(ValueArrayStorage<>);
+            else if (genericDefinition == typeof(ValueListStorage<>))
+                return typeof(ValueListStorage<>);
+
+            return null;
+        }
+
+        public static Type? GetGenericStorageTypeDefinition<T>()
+            => GetGenericStorageTypeDefinition(typeof(T));
+
+        public static Type GetGenericStorageTypeDefinition(this Metadata metadata)
+        {
+            if (metadata.TelemetryType != TelemetryType.Channel)
+                throw new NotImplementedException("Only channels are impleneted");
+            return metadata.Indexed ?? false ? typeof(ValueArrayStorage<>) : typeof(ValueStorage<>);
+        }
+
+        public static bool IsStorageType(this Type type) =>
+            GetGenericStorageTypeDefinition(type) is not null;
+
+        public static bool IsStorageType<T>() =>
+            IsStorageType(typeof(T));
+
+        public static void ThrowIfInvalidTypeForID(this Type type, TelemetryID id, TrailerIndexOrCount? trailerIndexOrCount = null)
+        {
+            trailerIndexOrCount ??= new(false, 1);
+            if (!type.IsGenericType)
+                throw new ArgumentException("Invalid type, all storage types are generic", nameof(type));
+
+            if (Metadata.ByID(id) is not Metadata metadata)
+                throw new ArgumentException("Invalid TelemetryID", nameof(id));
+
+            if (metadata.TelemetryType != TelemetryType.Channel)
+                throw new NotImplementedException("Only channel TelemetryTypes are implemented.");
+
+            if ((metadata.TrailerChannel ?? false) && trailerIndexOrCount.Value.IsCount)
+            {
+                if (!type.IsArray)
+                    throw new ArgumentException("Type must be an array for requesting trailer channel for multiple trailers.", nameof(type));
+                type = type.GetElementType() ?? throw new InvalidProgramException();
+            }
+
+            Type? storageType = type.GetGenericStorageTypeDefinition();
+            if ((metadata.Indexed ?? true) && storageType != typeof(ValueArrayStorage<>))
+                throw new ArgumentException($"Value storage for {id} must be a {typeof(ValueArrayStorage<>)}");
+            else if (!(metadata.Indexed ?? true) && storageType != typeof(ValueStorage<>))
+                throw new ArgumentException($"Value storage for {id} must be a {typeof(ValueStorage<>)}");
+        }
+
+        public static void ThrowIfInvalidTypeForID<T>(this TelemetryID id, TrailerIndexOrCount? trailerIndexOrCount = null) =>
+            ThrowIfInvalidTypeForID(typeof(T), id, trailerIndexOrCount);
+
+        public static int StorageFromBytes<T>(this ref T storage, SCSValueType valueType, byte[] bytes, int offset = 0) where T : struct
+        {
+            if (bytes.Length <= offset)
+                throw new NotImplementedException();
+
+            object boxed = storage;
+            int read;
+            Type? definingType = GetGenericStorageTypeDefinition<T>();
+
+            if (definingType == typeof(ValueStorage<>))
+            {
+                typeof(T).GetField("Initialized")!.SetValue(boxed, bytes[offset] > 0);
+
+                object value;
+                read = 1 + bytes.FromBytes(valueType, offset + 1, out value);
+                typeof(T).GetField("Value")!.SetValue(boxed, value);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
+
+            storage = (T)boxed;
+            return 1 + read;
+        }
+    }
 }
