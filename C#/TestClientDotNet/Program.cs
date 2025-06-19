dabad97d6388f78805f8e8e3dc9d87d2f753aa32
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-19 19:17:41 -0400

    Extracted test client code to a function

--- C#/TestClientDotNet/Program.cs
+++ C#/TestClientDotNet/Program.cs
@@ -1,40 +1,47 @@
 ﻿using TruckConnect;
 
 TimeSpan runFor = TimeSpan.FromMinutes(2);
+TimeSpan refreshRate = TimeSpan.FromMilliseconds(1000 / 25);
 
-GameStatus gameStatus = new();
-DataDefinition gaugeClusterDefinition = DataDefinition.Define<GaugeCluster>(2);
-Connection connection = new("127.0.0.1");
-connection.Connect();
-await connection.RegisterDataDefinitionAsync(gameStatus);
-await connection.RegisterDataDefinitionAsync(gaugeClusterDefinition);
+await DefinitionTest();
 
-DateTime start = DateTime.Now;
-while (DateTime.Now - start < runFor)
+async Task DefinitionTest()
 {
-    await connection.RequestAsync(gameStatus);
-    if (gameStatus.Paused.Value)
+    GameStatus gameStatus = new();
+    DataDefinition gaugeClusterDefinition = DataDefinition.Define<GaugeCluster>(2);
+
+    Connection connection = new("127.0.0.1");
+    await connection.ConnectAsync();
+    await connection.RegisterDataDefinitionAsync(gameStatus);
+    await connection.RegisterDataDefinitionAsync(gaugeClusterDefinition);
+
+    DateTime start = DateTime.Now;
+    while (DateTime.Now - start < runFor)
     {
-        Console.WriteLine(".");
-        continue;
+        await connection.RequestAsync(gameStatus);
+        if (gameStatus.Paused.Value)
+        {
+            Console.WriteLine(".");
+            continue;
+        }
+
+        GaugeCluster gaugeCluster = await connection.RequestAsync<GaugeCluster>(gaugeClusterDefinition);
+
+        Console.Write($"{(gameStatus.LocalScale.Initialized ? gameStatus.LocalScale.Value : "---")} | {(gameStatus.GameTime.Initialized ? gameStatus.GameTime.Value : "---")} | Trailers: ");
+        foreach (ValueStorage<bool> trailerConnected in gameStatus.TrailersConencted)
+            Console.Write(trailerConnected.Initialized ? (trailerConnected.Value ? "." : "x") : "?");
+        Console.WriteLine();
+
+        Console.Write($"{(gaugeCluster.engineRpm.Initialized ? gaugeCluster.engineRpm.Value.ToString("F0") : "---")}rpm | ");
+        Console.Write($"{(gaugeCluster.speed.Initialized ? gaugeCluster.speed.Value.ToString("F2") : "---")}m/s | ");
+        Console.Write($"{(gaugeCluster.lightLBlinker.Initialized ? (gaugeCluster.lightLBlinker.Value ? "<" : "-") : "?")}-");
+        Console.WriteLine($"{(gaugeCluster.lightRBlinker.Initialized ? (gaugeCluster.lightRBlinker.Value ? ">" : "-") : "?")}");
+        Console.Out.Flush();
+        Thread.Sleep((int)refreshRate.TotalMilliseconds);
     }
-
-    Console.Write($"{(gameStatus.LocalScale.Initialized ? gameStatus.LocalScale.Value : "---")} | {(gameStatus.GameTime.Initialized ? gameStatus.GameTime.Value : "---")} | Trailers: ");
-    foreach (ValueStorage<bool> trailerConnected in gameStatus.TrailersConencted)
-        Console.Write(trailerConnected.Initialized ? (trailerConnected.Value ? "." : "x") : "?");
-    Console.WriteLine();
-
-    GaugeCluster gaugeCluster = await connection.RequestAsync<GaugeCluster>(gaugeClusterDefinition);
-    Console.Write($"{(gaugeCluster.engineRpm.Initialized ? gaugeCluster.engineRpm.Value.ToString("F0") : "---")}rpm | ");
-    Console.Write($"{(gaugeCluster.speed.Initialized ? gaugeCluster.speed.Value.ToString("F2") : "---")}m/s | ");
-    Console.Write($"{(gaugeCluster.lightLBlinker.Initialized ? (gaugeCluster.lightLBlinker.Value ? "<" : "-") : "?")}-");
-    Console.WriteLine($"{(gaugeCluster.lightRBlinker.Initialized ? (gaugeCluster.lightRBlinker.Value ? ">" : "-") : "?")}");
-    Console.Out.Flush();
-    Thread.Sleep(1000 / 25);
+    await connection.UnregisterDataDefinitionAsync(gameStatus);
+    connection.Disconnect();
 }
-await connection.UnregisterDataDefinitionAsync(gameStatus);
-connection.Disconnect();
-
 
 class GameStatus : DataDefinition
 {
