b1a90857b6dc90f6c56c9872a9887842b33e0384
Franklyn Vasquez <franklynvasquez24@gmail.com>
2025-06-19 21:56:25 -0400

    Added MasterStructure

---
+++ C#/TruckConnectDotNet/MasterStructure.cs
@@ -0,0 +1,509 @@
+namespace TruckConnect
+{
+	public struct MasterStorage()
+	{
+		public ConfigurationStorage Configuration = new();
+		public struct ConfigurationStorage()
+		{
+			public ConfigurationSubstancesStorage ConfigurationSubstancesInfo = new();
+			public struct ConfigurationSubstancesStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueListStorage<String> Id = new();
+			}
+
+			public ConfigurationControlsStorage ConfigurationControlsInfo = new();
+			public struct ConfigurationControlsStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<String> ShifterType = new();
+			}
+
+			public ConfigurationHshifterStorage ConfigurationHshifterInfo = new();
+			public struct ConfigurationHshifterStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<UInt32> SelectorCount = new();
+
+				public ValueListStorage<Int32> SlotGear = new();
+
+				public ValueListStorage<UInt32> SlotHandlePosition = new();
+
+				public ValueListStorage<UInt32> SlotSelectors = new();
+			}
+
+			public ConfigurationTruckStorage ConfigurationTruckInfo = new();
+			public struct ConfigurationTruckStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<String> BrandId = new();
+
+				public ValueStorage<String> Brand = new();
+
+				public ValueStorage<String> Id = new();
+
+				public ValueStorage<String> Name = new();
+
+				public ValueStorage<Single> FuelCapacity = new();
+
+				public ValueStorage<Single> FuelWarningFactor = new();
+
+				public ValueStorage<Single> AdblueCapacity = new();
+
+				public ValueStorage<Single> AdblueWarningFactor = new();
+
+				public ValueStorage<Single> AirPressureWarning = new();
+
+				public ValueStorage<Single> AirPressureEmergency = new();
+
+				public ValueStorage<Single> OilPressureWarning = new();
+
+				public ValueStorage<Single> WaterTemperatureWarning = new();
+
+				public ValueStorage<Single> BatteryVoltageWarning = new();
+
+				public ValueStorage<Single> RpmLimit = new();
+
+				public ValueStorage<UInt32> ForwardGearCount = new();
+
+				public ValueStorage<UInt32> ReverseGearCount = new();
+
+				public ValueStorage<Single> DifferentialRatio = new();
+
+				public ValueStorage<UInt32> RetarderStepCount = new();
+
+				public ValueStorage<SCSValueFVector> CabinPosition = new();
+
+				public ValueListStorage<Single> ForwardRatio = new();
+
+				public ValueListStorage<Single> ReverseRatio = new();
+
+				public ValueStorage<SCSValueFVector> HeadPosition = new();
+
+				public ValueStorage<SCSValueFVector> HookPosition = new();
+
+				public ValueStorage<String> LicensePlate = new();
+
+				public ValueStorage<String> LicensePlateCountry = new();
+
+				public ValueStorage<String> LicensePlateCountryId = new();
+
+				public ValueStorage<UInt32> WheelCount = new();
+
+				public ValueListStorage<SCSValueFVector> WheelPosition = new();
+
+				public ValueListStorage<Boolean> WheelSteerable = new();
+
+				public ValueListStorage<Boolean> WheelSimulated = new();
+
+				public ValueListStorage<Single> WheelRadius = new();
+
+				public ValueListStorage<Boolean> WheelPowered = new();
+
+				public ValueListStorage<Boolean> WheelLiftable = new();
+			}
+
+			public ConfigurationTrailerStorage[] ConfigurationTrailerInfo = new ConfigurationTrailerStorage[10];
+			public struct ConfigurationTrailerStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<String> Id = new();
+
+				public ValueStorage<String> CargoAccessoryId = new();
+
+				public ValueStorage<SCSValueFVector> HookPosition = new();
+
+				public ValueStorage<String> BrandId = new();
+
+				public ValueStorage<String> Brand = new();
+
+				public ValueStorage<String> Name = new();
+
+				public ValueStorage<String> ChainType = new();
+
+				public ValueStorage<String> BodyType = new();
+
+				public ValueStorage<String> LicensePlate = new();
+
+				public ValueStorage<String> LicensePlateCountry = new();
+
+				public ValueStorage<String> LicensePlateCountryId = new();
+
+				public ValueStorage<UInt32> WheelCount = new();
+
+				public ValueListStorage<SCSValueFVector> WheelPosition = new();
+
+				public ValueListStorage<Boolean> WheelSteerable = new();
+
+				public ValueListStorage<Boolean> WheelSimulated = new();
+
+				public ValueListStorage<Single> WheelRadius = new();
+
+				public ValueListStorage<Boolean> WheelPowered = new();
+
+				public ValueListStorage<Boolean> WheelLiftable = new();
+			}
+
+			public ConfigurationJobStorage ConfigurationJobInfo = new();
+			public struct ConfigurationJobStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<String> CargoId = new();
+
+				public ValueStorage<String> Cargo = new();
+
+				public ValueStorage<Single> CargoMass = new();
+
+				public ValueStorage<String> DestinationCityId = new();
+
+				public ValueStorage<Single> CargoUnitMass = new();
+
+				public ValueStorage<UInt32> CargoUnitCount = new();
+
+				public ValueStorage<String> DestinationCity = new();
+
+				public ValueStorage<String> SourceCityId = new();
+
+				public ValueStorage<String> SourceCity = new();
+
+				public ValueStorage<String> DestinationCompanyId = new();
+
+				public ValueStorage<String> DestinationCompany = new();
+
+				public ValueStorage<String> SourceCompanyId = new();
+
+				public ValueStorage<String> SourceCompany = new();
+
+				public ValueStorage<UInt64> Income = new();
+
+				public ValueStorage<UInt32> DeliveryTime = new();
+
+				public ValueStorage<Boolean> IsCargoLoaded = new();
+
+				public ValueStorage<String> JobMarket = new();
+
+				public ValueStorage<Boolean> SpecialJob = new();
+
+				public ValueStorage<UInt32> PlannedDistanceKm = new();
+			}
+		}
+
+		public GameplayStorage Gameplay = new();
+		public struct GameplayStorage()
+		{
+			public GameplayJobCancelledStorage GameplayJobCancelledInfo = new();
+			public struct GameplayJobCancelledStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<Int64> CancelPenalty = new();
+			}
+
+			public GameplayJobDeliveredStorage GameplayJobDeliveredInfo = new();
+			public struct GameplayJobDeliveredStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<Int64> Revenue = new();
+
+				public ValueStorage<Int32> EarnedXp = new();
+
+				public ValueStorage<Single> CargoDamage = new();
+
+				public ValueStorage<Single> DistanceKm = new();
+
+				public ValueStorage<UInt32> DeliveryTime = new();
+
+				public ValueStorage<Boolean> AutoParkUsed = new();
+
+				public ValueStorage<Boolean> AutoLoadUsed = new();
+			}
+
+			public GameplayPlayerFinedStorage GameplayPlayerFinedInfo = new();
+			public struct GameplayPlayerFinedStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<String> FineOffence = new();
+
+				public ValueStorage<Int64> FineAmount = new();
+			}
+
+			public GameplayPlayerTollgatePaidStorage GameplayPlayerTollgatePaidInfo = new();
+			public struct GameplayPlayerTollgatePaidStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<Int64> PayAmount = new();
+			}
+
+			public GameplayPlayerUseFerryStorage GameplayPlayerUseFerryInfo = new();
+			public struct GameplayPlayerUseFerryStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<Int64> PayAmount = new();
+
+				public ValueStorage<String> SourceName = new();
+
+				public ValueStorage<String> TargetName = new();
+
+				public ValueStorage<String> SourceId = new();
+
+				public ValueStorage<String> TargetId = new();
+			}
+
+			public GameplayPlayerUseTrainStorage GameplayPlayerUseTrainInfo = new();
+			public struct GameplayPlayerUseTrainStorage()
+			{
+				public ValueStorage<UInt32> Latest = new();
+
+				public ValueStorage<Int64> PayAmount = new();
+
+				public ValueStorage<String> SourceName = new();
+
+				public ValueStorage<String> TargetName = new();
+
+				public ValueStorage<String> SourceId = new();
+
+				public ValueStorage<String> TargetId = new();
+			}
+		}
+
+		public ChannelsStorage Channels = new();
+		public struct ChannelsStorage()
+		{
+			public GeneralStorage General = new();
+			public struct GeneralStorage()
+			{
+				public ValueStorage<Boolean> ChannelPaused = new();
+
+				public ValueStorage<Single> ChannelLocalScale = new();
+
+				public ValueStorage<UInt32> ChannelGameTime = new();
+
+				public ValueStorage<Int32> ChannelMultiplayerTimeOffset = new();
+
+				public ValueStorage<Int32> ChannelNextRestStop = new();
+
+				public ValueStorage<Single> JobChannelCargoDamage = new();
+			}
+
+			public TruckStorage Truck = new();
+			public struct TruckStorage()
+			{
+				public ValueStorage<SCSValueDPlacement> TruckChannelWorldPlacement = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelLocalLinearVelocity = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelLocalAngularVelocity = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelLocalLinearAcceleration = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelLocalAngularAcceleration = new();
+
+				public ValueStorage<SCSValueFPlacement> TruckChannelCabinOffset = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelCabinAngularVelocity = new();
+
+				public ValueStorage<SCSValueFVector> TruckChannelCabinAngularAcceleration = new();
+
+				public ValueStorage<SCSValueFPlacement> TruckChannelHeadOffset = new();
+
+				public ValueStorage<Single> TruckChannelSpeed = new();
+
+				public ValueStorage<Single> TruckChannelEngineRpm = new();
+
+				public ValueStorage<Int32> TruckChannelEngineGear = new();
+
+				public ValueStorage<Int32> TruckChannelDisplayedGear = new();
+
+				public ValueStorage<Single> TruckChannelInputSteering = new();
+
+				public ValueStorage<Single> TruckChannelInputThrottle = new();
+
+				public ValueStorage<Single> TruckChannelInputBrake = new();
+
+				public ValueStorage<Single> TruckChannelInputClutch = new();
+
+				public ValueStorage<Single> TruckChannelEffectiveSteering = new();
+
+				public ValueStorage<Single> TruckChannelEffectiveThrottle = new();
+
+				public ValueStorage<Single> TruckChannelEffectiveBrake = new();
+
+				public ValueStorage<Single> TruckChannelEffectiveClutch = new();
+
+				public ValueStorage<Single> TruckChannelCruiseControl = new();
+
+				public ValueStorage<UInt32> TruckChannelHshifterSlot = new();
+
+				public ValueArrayStorage<Boolean> TruckChannelHshifterSelector = new();
+
+				public ValueStorage<Boolean> TruckChannelParkingBrake = new();
+
+				public ValueStorage<Boolean> TruckChannelMotorBrake = new();
+
+				public ValueStorage<UInt32> TruckChannelRetarderLevel = new();
+
+				public ValueStorage<Single> TruckChannelBrakeAirPressure = new();
+
+				public ValueStorage<Boolean> TruckChannelBrakeAirPressureWarning = new();
+
+				public ValueStorage<Boolean> TruckChannelBrakeAirPressureEmergency = new();
+
+				public ValueStorage<Single> TruckChannelBrakeTemperature = new();
+
+				public ValueStorage<Single> TruckChannelFuel = new();
+
+				public ValueStorage<Boolean> TruckChannelFuelWarning = new();
+
+				public ValueStorage<Single> TruckChannelFuelAverageConsumption = new();
+
+				public ValueStorage<Single> TruckChannelFuelRange = new();
+
+				public ValueStorage<Single> TruckChannelAdblue = new();
+
+				public ValueStorage<Boolean> TruckChannelAdblueWarning = new();
+
+				public ValueStorage<Single> TruckChannelOilPressure = new();
+
+				public ValueStorage<Boolean> TruckChannelOilPressureWarning = new();
+
+				public ValueStorage<Single> TruckChannelOilTemperature = new();
+
+				public ValueStorage<Single> TruckChannelWaterTemperature = new();
+
+				public ValueStorage<Boolean> TruckChannelWaterTemperatureWarning = new();
+
+				public ValueStorage<Single> TruckChannelBatteryVoltage = new();
+
+				public ValueStorage<Boolean> TruckChannelBatteryVoltageWarning = new();
+
+				public ValueStorage<Boolean> TruckChannelElectricEnabled = new();
+
+				public ValueStorage<Boolean> TruckChannelEngineEnabled = new();
+
+				public ValueStorage<Boolean> TruckChannelLblinker = new();
+
+				public ValueStorage<Boolean> TruckChannelRblinker = new();
+
+				public ValueStorage<Boolean> TruckChannelHazardWarning = new();
+
+				public ValueStorage<Boolean> TruckChannelLightLblinker = new();
+
+				public ValueStorage<Boolean> TruckChannelLightRblinker = new();
+
+				public ValueStorage<Boolean> TruckChannelLightParking = new();
+
+				public ValueStorage<Boolean> TruckChannelLightLowBeam = new();
+
+				public ValueStorage<Boolean> TruckChannelLightHighBeam = new();
+
+				public ValueStorage<UInt32> TruckChannelLightAuxFront = new();
+
+				public ValueStorage<UInt32> TruckChannelLightAuxRoof = new();
+
+				public ValueStorage<Boolean> TruckChannelLightBeacon = new();
+
+				public ValueStorage<Boolean> TruckChannelLightBrake = new();
+
+				public ValueStorage<Boolean> TruckChannelLightReverse = new();
+
+				public ValueStorage<Boolean> TruckChannelWipers = new();
+
+				public ValueStorage<Single> TruckChannelDashboardBacklight = new();
+
+				public ValueStorage<Boolean> TruckChannelDifferentialLock = new();
+
+				public ValueStorage<Boolean> TruckChannelLiftAxle = new();
+
+				public ValueStorage<Boolean> TruckChannelLiftAxleIndicator = new();
+
+				public ValueStorage<Boolean> TruckChannelTrailerLiftAxle = new();
+
+				public ValueStorage<Boolean> TruckChannelTrailerLiftAxleIndicator = new();
+
+				public ValueStorage<Single> TruckChannelWearEngine = new();
+
+				public ValueStorage<Single> TruckChannelWearTransmission = new();
+
+				public ValueStorage<Single> TruckChannelWearCabin = new();
+
+				public ValueStorage<Single> TruckChannelWearChassis = new();
+
+				public ValueStorage<Single> TruckChannelWearWheels = new();
+
+				public ValueStorage<Single> TruckChannelOdometer = new();
+
+				public ValueStorage<Single> TruckChannelNavigationDistance = new();
+
+				public ValueStorage<Single> TruckChannelNavigationTime = new();
+
+				public ValueStorage<Single> TruckChannelNavigationSpeedLimit = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelSuspDeflection = new();
+
+				public ValueArrayStorage<Boolean> TruckChannelWheelOnGround = new();
+
+				public ValueArrayStorage<UInt32> TruckChannelWheelSubstance = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelVelocity = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelSteering = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelRotation = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelLift = new();
+
+				public ValueArrayStorage<Single> TruckChannelWheelLiftOffset = new();
+			}
+
+			public TrailerStorage[] Trailer = new TrailerStorage[10];
+			public struct TrailerStorage()
+			{
+				public ValueStorage<Boolean> TrailerChannelConnected = new();
+
+				public ValueStorage<Single> TrailerChannelCargoDamage = new();
+
+				public ValueStorage<SCSValueDPlacement> TrailerChannelWorldPlacement = new();
+
+				public ValueStorage<SCSValueFVector> TrailerChannelLocalLinearVelocity = new();
+
+				public ValueStorage<SCSValueFVector> TrailerChannelLocalAngularVelocity = new();
+
+				public ValueStorage<SCSValueFVector> TrailerChannelLocalLinearAcceleration = new();
+
+				public ValueStorage<SCSValueFVector> TrailerChannelLocalAngularAcceleration = new();
+
+				public ValueStorage<Single> TrailerChannelWearBody = new();
+
+				public ValueStorage<Single> TrailerChannelWearChassis = new();
+
+				public ValueStorage<Single> TrailerChannelWearWheels = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelSuspDeflection = new();
+
+				public ValueArrayStorage<Boolean> TrailerChannelWheelOnGround = new();
+
+				public ValueArrayStorage<UInt32> TrailerChannelWheelSubstance = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelVelocity = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelSteering = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelRotation = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelLift = new();
+
+				public ValueArrayStorage<Single> TrailerChannelWheelLiftOffset = new();
+			}
+		}
+	}
+}
