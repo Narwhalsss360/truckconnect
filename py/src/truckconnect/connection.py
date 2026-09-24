3c5dfa27bdae2667556ad40d01f18e1ff66d60cd
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-09-24 22:59:22 +0300

    FIX: removed extra bad import

--- py/src/truckconnect/connection.py
+++ py/src/truckconnect/connection.py
@@ -6,7 +6,7 @@ from socket import socket, AddressFamily, SocketKind, IPPROTO_TCP, SHUT_RDWR
 from typing import Callable, Type, TypeVar
 from truckconnect.telemetry_id import TelemetryID
 from scssdk_truckconnect.truckconnect import Version, Telemetry, telemetries, TelemetryType
-from nstreamcom import Collector, CollectorState, CollectorStates, encode_with_size
+from nstreamcom import Collector, CollectorStates, encode_with_size
 from truckconnect.value_storage import BufferType, value_storage_from_bytes, value_array_storage_from_bytes, SCSValueType
 from .data import DATA_DEFINITION_ATTR_NAME, NON_CHANNEL_TYPES, DataDefinition, DeserializedType, NON_CHANNEL_DESERIALIZERS
 
